8bdb31594eb9c7e94f2b118f52544b180abb3ff3
449sabu <449sabu@gmail.com>
2025-10-23 02:37:18 +0900

    Dashboard Page 作成

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,9 @@
-import { index, type RouteConfig, route } from '@react-router/dev/routes';
+import { index, layout, type RouteConfig, route } from '@react-router/dev/routes';
 
-export default [index('routes/home.tsx'), route('/signup', 'routes/signup.tsx')] satisfies RouteConfig;
+export default [
+	layout('routes/layout.tsx', [
+		index('routes/home.tsx'),
+		route('/signup', 'routes/signup.tsx'),
+		route('/dashboard', 'routes/dashboard.tsx'),
+	]),
+] satisfies RouteConfig;
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -13,16 +13,20 @@ export function loader({ context }: Route.LoaderArgs) {
 export default function Home({ loaderData }: Route.ComponentProps) {
 	const { message } = loaderData;
 	return (
-		<div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
-			<div className="flex w-full max-w-sm flex-col gap-6">
-				<Link to="/signup" className="flex items-center gap-2 self-center font-medium">
-					<div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
-						<GalleryVerticalEnd className="size-4" />
-					</div>
-					Sign Up
-				</Link>
-				<p>{message}</p>
-			</div>
+		<div>
+			<Link to="/signup" className="flex items-center gap-2 self-center font-medium">
+				<div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
+					<GalleryVerticalEnd className="size-4" />
+				</div>
+				Sign Up
+			</Link>
+			<Link to="/dashboard" className="flex items-center gap-2 self-center font-medium">
+				<div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
+					<GalleryVerticalEnd className="size-4" />
+				</div>
+				Look at Dashboard
+			</Link>
+			<p>{message}</p>
 		</div>
 	);
 }
