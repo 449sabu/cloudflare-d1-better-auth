71e7145ff5f18b75fc14351b97936818187d9a57
449sabu <449sabu@gmail.com>
2025-10-23 23:00:08 +0900

    actionsでデータの書き込み・読み込みテスト

--- app/routes/dashboard.tsx
+++ app/routes/dashboard.tsx
@@ -1,7 +1,61 @@
-export default function Dashboard() {
+import { eq } from 'drizzle-orm';
+import { drizzle } from 'drizzle-orm/d1';
+import { useId } from 'react';
+import { Form } from 'react-router';
+import { Button } from '~/components/ui/button';
+import { Input } from '~/components/ui/input';
+import { pointTable } from '../../schema/point';
+import type { Route } from './+types/dashboard';
+
+export async function loader({ context }: Route.LoaderArgs) {
+	const db = drizzle(context.cloudflare.env.sample_db);
+	const points = await db.select().from(pointTable);
+
+	console.log(points);
+
+	return {
+		message: context.cloudflare.env.VALUE_FROM_CLOUDFLARE,
+		points,
+	};
+}
+
+export default function Dashboard({ loaderData }: Route.ComponentProps) {
+	const { points } = loaderData;
+	const nameId = useId();
+	const deleteNameId = useId();
+
 	return (
 		<div>
 			<h1 className="text-center">Dashboard</h1>
+			<Form method="post">
+				<Input id={nameId} name="name" />
+				<Button type="submit">作成</Button>
+			</Form>
+			<ul>
+				{points.map((point) => (
+					<li key={point.id}>{point.name}</li>
+				))}
+			</ul>
+			<Form method="post">
+				<Input id={deleteNameId} name="deleteName" />
+				<Button type="submit">削除</Button>
+			</Form>
 		</div>
 	);
+}
+
+export async function action({ request, context }: Route.ActionArgs) {
+	const db = drizzle(context.cloudflare.env.sample_db);
+	const formData = await request.formData();
+
+	const name = formData.get('name');
+	const deleteName = formData.get('deleteName');
+
+	if (name) {
+		await db.insert(pointTable).values({ name: name as string });
+	}
+	if (deleteName) {
+		await db.delete(pointTable).where(eq(pointTable.name, deleteName as string));
+	}
+	return { name, deleteName };
 }
