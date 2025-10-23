5241b8f9781dcbf006369d9e2f5f6c249c3a2a11
449sabu <449sabu@gmail.com>
2025-10-23 18:13:17 +0900

    ナビゲーションの作成

--- app/routes/signup.tsx
+++ app/routes/signup.tsx
@@ -1,5 +1,3 @@
-import { GalleryVerticalEnd } from 'lucide-react';
-import { Link } from 'react-router';
 import { SignupForm } from '~/components/signup-form';
 
 export default function SignupPage() {
